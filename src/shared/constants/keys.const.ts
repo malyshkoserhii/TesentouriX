41e89d683444cb2542bc500ad22eb7fc7590636f
Serhii Malyshko <malyshkosergiy@gmail.com>
2024-04-26 17:46:21 +0300

    fix: Fixed onboarding

--- src/shared/constants/keys.const.ts
+++ src/shared/constants/keys.const.ts
@@ -3,6 +3,7 @@ import { BudgetType } from '../types';
 export const STORAGE_KEYS = Object.freeze({
 	NOTES: 'NOTES',
 	BUDGETS: 'BUDGETS',
+	ONBOARDING: 'ONBOARDING',
 });
 
 export const BUDGET_TYPE = {
